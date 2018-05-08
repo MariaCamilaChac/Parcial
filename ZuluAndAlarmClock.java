7c79534cb2eb34a3e4b8249e6650a88024b20291
MariaCamilaChac <32178494+MariaCamilaChac@users.noreply.github.com>
2018-05-08 10:48:16 -0500

    Update ZuluAndAlarmClock.java

--- ZuluAndAlarmClock.java
+++ ZuluAndAlarmClock.java
@@ -6,116 +6,143 @@ import java.io.OutputStreamWriter;
 
 public class ZuluAndAlarmClock {
 
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input;
-        /*T test cases N numbers of clocks K number of alarms, operation for the number of operations for each clock configuration
-          temp for save temporally the number to increase or decrease.*/
-        int T, N, K, operation = 0, temp[] = new int[3];
-        /*smallers for the configuration of alarm with less steps, smaller for the comparision to find the smaller value.*/
-        int smallers[], smaller = 0;
-        /*index for the loops*/
-        int i, j, m = 0;
-        /*this matrix for the clocks and alarms, operations for the counters of the operations of each alarm configuration.*/
-        int clocks[][], alarms[][], operations[][];
+	public static void main(String[] args) throws IOException {
+        	
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+      			
+    int operation = 0, temp[] = new int[3];
+     
+    int smaller = 0;
+   
+       
+    int T = Integer.parseInt(br.readLine());
+    String input = br.readLine();
 
-        T = Integer.parseInt(br.readLine());
-        input = br.readLine();
+    int N = Integer.parseInt(input.split(" ")[0]);
+    int K = Integer.parseInt(input.split(" ")[1]);
 
-        N = Integer.parseInt(input.split(" ")[0]);
-        K = Integer.parseInt(input.split(" ")[1]);
+    int[][] clocks = new int[N][3]; 
+    
+    int[][] alarms = new int[K][3];
+        
+    int[][] operations = new int[N][K]; // a counter for each clock configuration to set alarm
+        
+    int []smallers = new int[K];
 
-        clocks = new int[N][3]; //3 is for the units of time.
-        alarms = new int[K][3];//3 is for the units of time.
-        operations = new int[N][K]; // a counter for each clock configuration to set alarm
-        smallers = new int[K];
-
-        for (i = 0; i < N; i++) { //for save the clocks
-            input = br.readLine();
-            for (j = 0; j < 3; j++) {
-                clocks[i][j] = Integer.parseInt(input.split(":")[j]);
-            }
+    for (int i = 0; i < N; i++) { //input of the user
+    	
+    	input = br.readLine();
+    	
+    	for (int j = 0; j < 3; j++) {
+    		clocks[i][j] = Integer.parseInt(input.split(":")[j]);
         }
-
-        for (i = 0; i < K; i++) { //for save the alarms
-            input = br.readLine();
-            for (j = 0; j < 3; j++) {
-                alarms[i][j] = Integer.parseInt(input.split(":")[j]);
-            }
-        }
-
-        while (T-- > 0) {
-            for (i = 0; i < N; i++) { //the clocks
-                for (j = 0; j < K; j++) { //the alarms
-                    temp = clocks[i].clone();
-                    for (m = 2; m >= 0; m--) { //units of time
-                        if (temp[m] != alarms[j][m]) {
-                            if (m == 0) { // if we are comparing hours
-                                if ((temp[m] > alarms[j][m] && Math.abs(temp[m] - alarms[j][m]) < 12) || 
-                                     (temp[m] < alarms[j][m] && Math.abs(temp[m] - alarms[j][m]) > 12) ) {
-                                    while (temp[m] != alarms[j][m]) {
-                                        temp[m]--;
-                                        operation++;
-                                        if (temp[m] == -1) {
-                                            temp[m] = 23;
-                                        }
-                                    }
-                                } else {
-                                    while (temp[m] != alarms[j][m]) {
-                                        temp[m]++;
-                                        operation++;
-                                        if (temp[m] == 24) {
-                                            temp[m] = 0;
-                                        }
-                                    }
-                                }
-                            } else {
-                                if ((temp[m] > alarms[j][m] && Math.abs(temp[m] - alarms[j][m]) < 30) || 
-                                     (temp[m] < alarms[j][m] && Math.abs(temp[m] - alarms[j][m]) > 30)) {
-                                    while (temp[m] != alarms[j][m]) {
-                                        temp[m]--;
-                                        operation++;
-                                        if (temp[m] == -1) {
-                                            temp[m] = 59;
-                                            temp[m - 1]--;
-                                        }
-                                    }
-                                } else {
-                                    while (temp[m] != alarms[j][m]) {
-                                        temp[m]++;
-                                        operation++;
-                                        if (temp[m] == 60) {
-                                            temp[m] = 0;
-                                            temp[m - 1]++;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    operations[i][j] = operation;
-                    operation = 0;
-                }
-            }
-
-            for (j = 0; j < K; j++) { //find the smallers value for each alarm configuration.
-                smaller = operations[0][j];
-                for (i = 0; i < N; i++) {
-                    if (operations[i][j] < smaller) {
-                        smaller = operations[i][j];
-                    }
-                }
-                smallers[j] = smaller;
-            }
-
-            smaller = 0;
-            for (j = 0; j < K; j++) {
-                smaller += smallers[j];
-            }
-
-            bw.write(smaller + "\n");
-            bw.flush();
-        }
+        	
     }
+
+    for (int i = 0; i < K; i++) { //for save the alarms
+    	input = br.readLine();
+    	for (int j = 0; j < 3; j++) {
+    		alarms[i][j] = Integer.parseInt(input.split(":")[j]);    	
+    	}
+    }
+
+    while (T-- > 0) {
+    	for (int i = 0; i < N; i++) { //the clocks
+    		
+    		for (int j = 0; j < K; j++) { //the alarms
+    			temp = clocks[i].clone();
+    		
+    			for (int m = 2; m >= 0; m--) { //units of time
+    			
+    				if (temp[m] != alarms[j][m]) {
+    				
+    					if (m == 0) { // if we are comparing hours
+    					
+    						if ((temp[m] > alarms[j][m] && Math.abs(temp[m] - alarms[j][m]) < 12) || 	
+    								(temp[m] < alarms[j][m] && Math.abs(temp[m] - alarms[j][m]) > 12) ) {
+    						
+    							while (temp[m] != alarms[j][m]) {
+    								temp[m]--;
+    								operation++;
+    							
+    								if (temp[m] == -1) {
+    									temp[m] = 23;
+    								}    
+    							}
+                            
+    						} 
+    						else {
+    							while (temp[m] != alarms[j][m]) {
+    								temp[m]++;
+    								operation++;
+    								
+    								if (temp[m] == 24) 
+    									temp[m] = 0;                        
+                                    }
+                                }
+                            } 
+    					else {
+    						if ((temp[m] > alarms[j][m] && Math.abs(temp[m] - alarms[j][m]) < 30) || 
+    							(temp[m] < alarms[j][m] && Math.abs(temp[m] - alarms[j][m]) > 30)) {
+                                    	
+    							while (temp[m] != alarms[j][m]) {
+    								temp[m]--;
+    								operation++;
+    								
+    								if (temp[m] == -1) {
+    									temp[m] = 59;
+    									temp[m - 1]--;   
+    								}
+                          		}      
+    						}
+    						else {
+                                 
+    							while (temp[m] != alarms[j][m]) {
+    								temp[m]++;
+    								operation++;
+    								if (temp[m] == 60) {
+    									temp[m] = 0;
+    									temp[m - 1]++;
+                                        
+    								}
+                                    
+    							}
+                                
+    						}
+                            
+    					}
+                        
+    				}
+                    
+    			}
+    			operations[i][j] = operation;
+    			operation = 0;
+                	
+    		}
+            
+    	}
+
+    	for (int j = 0; j < K; j++) { //find the smaller value for each alarm configuration.
+    		smaller = operations[0][j];
+    		for (int i = 0; i < N; i++) {
+    			if (operations[i][j] < smaller) 
+    				smaller = operations[i][j];
+                    	
+                }
+                	
+    		smallers[j] = smaller;
+            
+    	}
+
+    	smaller = 0;
+    	for (int j = 0; j < K; j++) 
+    		smaller += smallers[j];
+            	
+    	bw.write(smaller + "\n");
+    	bw.flush();
+       }
+    
+	}
 }
